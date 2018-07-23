30a1af59905209bf64d7b57b2c6743d6824dedcb
winsarrocena <wins.arrocena@gmail.com>
2018-07-24 07:15:04 +0800

    updates to scripts

--- firmware/Arduino_script/Arduino_script.ino
+++ firmware/Arduino_script/Arduino_script.ino
@@ -1,17 +1,24 @@
 //Disclaimer: this code is not tested, may not work, just for the purposes of this exercise
 #include <Wire.h>
 
+#define SLAVE_ADDR 	0x48
+#define BAUD_RATE	9600
+#define RELAY1		3
+#define RELAY2		5
+#define RPI_POWER_CTRL	7
+#define FUEL_SENSOR	A0
+
 char flag = 0;
 char counter = 0;
 
 void setup() {
-  pinMode(3, OUTPUT);          // sets the digital pin 3 as output
-  pinMode(5, OUTPUT);          // sets the digital pin 5 as output
-  pinMode(7, OUTPUT);          // sets the digital pin 7 as output
+  pinMode(RELAY1, OUTPUT);          // sets the digital pin 3 as output
+  pinMode(RELAY2, OUTPUT);          // sets the digital pin 5 as output
+  pinMode(RPI_POWER_CTRL, OUTPUT);          // sets the digital pin 7 as output
   
-  Wire.begin(8);                // join i2c bus with address #8
+  Wire.begin(SLAVE_ADDR);                // join i2c bus with address #8
   Wire.onReceive(receiveEvent); // register event
-  Serial.begin(9600);           // start serial for output
+  Serial.begin(BAUD_RATE);           // start serial for output
 }
 
 void loop() {
@@ -25,9 +32,9 @@ void loop() {
   }
   if(counter>100){ // > 10 seconds?
     // we will reset the Pi
-      digitalWrite(7, LOW);       // sets the digital pin 7 off
+      digitalWrite(RPI_POWER_CTRL, LOW);       // sets the RPI_POWER_CTRL off
       delay(100);
-      digitalWrite(7, HIGH);       // sets the digital pin 7 on
+      digitalWrite(RPI_POWER_CTRL, HIGH);       // sets the RPI_POWER_CTRL on
       counter=0;
       flag=0;
     }
@@ -35,34 +42,32 @@ void loop() {
 
 // function that executes whenever data is received from master
 // this function is registered as an event, see setup()
-void receiveEvent(int howMany) {
-  while (1 < Wire.available()) { // loop through all but the last
+void receiveEvent(int num) {
+  while (Wire.available()) { 
     flag = 1;
     char c = Wire.read(); // receive byte as a character
     Serial.println(c, HEX);         // print the char
     switch(c){
       case 0xFE: //read fuel sensor
       int rawValue;
-      rawValue = analogRead(A0); 
+      rawValue = analogRead(FUEL_SENSOR); 
       byte buffer[2];
       buffer[0] = rawValue >> 8;
       buffer[1] = rawValue & 0xFF;
-      // ?? need to test here, not very sure how this i2c works in arduino
-      Wire.beginTransmission(9); // transmit to device
+      // ?? need to test here, not very sure how this works in arduino
       Wire.write(buffer, 2); // sends buffer
-      Wire.endTransmission(); // stop transmitting
       break;
       case 0xA1: //on relay 1
-        digitalWrite(3, HIGH);       // sets the digital pin 3 on
+        digitalWrite(RELAY1, HIGH);       // sets the digital pin 3 on
       break;
       case 0xB1: //on relay 2
-        digitalWrite(5, HIGH);       // sets the digital pin 5 on
+        digitalWrite(RELAY2, HIGH);       // sets the digital pin 5 on
       break;
       case 0xA0: //off relay 1
-        digitalWrite(3, LOW);       // sets the digital pin 3 off
+        digitalWrite(RELAY1, LOW);       // sets the digital pin 3 off
       break;
       case 0xB0://off relay 2
-        digitalWrite(5, LOW);       // sets the digital pin 5 off
+        digitalWrite(RELAY2, LOW);       // sets the digital pin 5 off
       break;
       default:
       break;
--- firmware/RPi_script/rpi_script.py
+++ firmware/RPi_script/rpi_script.py
@@ -2,10 +2,35 @@ import serial
 import smbus
 import time
 
+bus = smbus.SMBus(1)
+arduino_addr = 0x48
 
-port = serial.Serial("/dev/ttyAMA0", baudrate=115200, timeout=3.0)
-
+sp = serial.Serial(
+        port='/dev/ttyACM0',
+        baudrate=9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+)
+sp.write("^RESET\r\n" )
 while True:
-    port.write("\r\nSay something:")
-    rcv = port.read(10)
-    port.write("\r\nYou sent:" + repr(rcv))
+    rcv = sp.readline()
+	if rcv == "AT+RLYON=1\r\n":
+		sp.write("^OK\r\n" )
+		bus.write_byte(arduino_addr,0xA1)
+	elif rcv == "AT+RLYON=2\r\n":
+		sp.write("^OK\r\n" )
+		bus.write_byte(arduino_addr,0xB1)
+	elif rcv == "AT+RLYOFF=1\r\n":
+		sp.write("^OK\r\n" )
+		bus.write_byte(arduino_addr,0xA0)
+	elif rcv == "AT+RLYOFF=2\r\n":
+		sp.write("^OK\r\n" )	
+		bus.write_byte(arduino_addr,0xB0)
+	elif rcv == "AT+SENS=?\r\n":
+		sp.write("^OK\r\n" )
+		bus.write_byte(arduino_addr,0xFE)
+		
+	else:
+		sp.write("^ERROR: Invalid command\r\n" )
